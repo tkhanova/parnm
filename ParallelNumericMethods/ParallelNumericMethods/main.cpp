4672ba8dafe87cd6f63cf65d6be0b4505951c904
unknown <khanova.tatiana@gmail.com>
2015-04-06 17:29:35 +0300

    Working version

--- ParallelNumericMethods/ParallelNumericMethods/main.cpp
+++ ParallelNumericMethods/ParallelNumericMethods/main.cpp
@@ -25,6 +25,57 @@ void sort ( int *col_idx, double *a, int start, int end ) {
             }
 }
 
+/**
+ * API
+ *   int StructTranspose(int n, int* column, int* row,
+ *                       int* &tColumn, int* &tRow);
+ *   “ранспонирование структуры матрицы
+ * INPUT
+ *   int  n        - размер матрицы
+ *   int* column   - CRS описание матрицы
+ *   int* row
+ * OUTPUT
+ *   int* &tColumn - CRS описание матрицы
+ *   int* &tRow
+ * RETURN
+ * возвращаетс€ код ошибки
+**/
+int StructTranspose ( int n, int* column, int* row, int* &tColumn, int* &tRow ) {
+    int i, j;
+    int nz;
+    int S;
+    nz = row[n];
+    tColumn = new int [nz];
+    tRow    = new int [n + 1];
+    memset ( tRow, 0, ( n + 1 ) * sizeof ( int ) );
+    
+    for ( i = 0; i < nz; i++ ) {
+        tRow[column[i] + 1]++;
+    }
+    
+    S = 0;
+    
+    for ( i = 1; i <= n; i++ ) {
+        int tmp = tRow[i];
+        tRow[i] = S;
+        S = S + tmp;
+    }
+    
+    for ( i = 0; i < n; i++ ) {
+        int j1 = row[i];
+        int j2 = row[i + 1];
+        int Col = i; // —толбец в AT - строка в ј
+        
+        for ( j = j1; j < j2; j++ ) {
+            int RIndex = column[j];  // —трока в AT
+            int IIndex = tRow[RIndex + 1];
+            tColumn[IIndex] = Col;
+            tRow   [RIndex + 1]++;
+        }
+    }
+    
+    return 0;
+}
 
 /* converts COO format to CSR format, in-place,
 if SORT_IN_ROW is defined, each row is sorted in column index.
@@ -93,25 +144,25 @@ struct CRS {
     int * col_ind;
     int * row_ptr;
     
-    int N, M, NZ;
+    int N, NZ;
     
     CRS ( char * filename ) {
         std::ifstream fin ( filename );
         
         while ( fin.peek() == '%' ) fin.ignore ( 2048, '\n' );
         
-        fin >> M >> N >> NZ;
-        cout << M << " " << N << " " << NZ << endl;
+        fin >>  N >> N >> NZ;
+        cout << N << " " << NZ << endl;
         val = new double[NZ];
         col_ind = new int[NZ];
-        row_ptr = new int[N + 1];
+        row_ptr = new int[NZ];
         
         // Read the data in coo
         for ( int i = 0; i < NZ; i++ )  {
             fin >> row_ptr[i] >> col_ind[i] >> val[i];
+            cout << row_ptr[i] << " " << col_ind[i] << " " << val[i] << endl;
             row_ptr[i] -= 1;  /* adjust from 1-based to 0-based */
             col_ind[i] -= 1;
-            cout << row_ptr[i] << " " << col_ind[i] << " " << val[i] << endl;
         }
         
         fin.close();
@@ -136,8 +187,32 @@ struct CRS {
         cout << endl;
     }
     
-    CRS() {}
+    CRS() : N ( 0 ), NZ ( 0 ) {}
+    
+    ~CRS() {
+        delete[] val;
+        delete[] col_ind;
+        delete[] row_ptr;
+    }
 };
+
+ostream & operator<< ( ostream & s, const CRS & m ) {
+    for ( int i = 0; i < m.NZ; i++ )
+        s << m.val[i] << " ";
+        
+    s << endl;
+    
+    for ( int i = 0; i < m.NZ; i++ )
+        s << m.col_ind[i] << " ";
+        
+    s << endl;
+    
+    for ( int i = 0; i < m.N + 1; i++ )
+        s << m.row_ptr[i] << " ";
+        
+    s << endl;
+    return s;
+}
 
 /** * API * int symbolicILUp(int p, int n, int * col, int * row, * int * lucol, int * lurow, double * &luval, * int * uptr, int &countL, int &countU);
 * символическа€ фаза ILU(p) * INPUT * int n - размер матрицы * матрица A * int * col - индексы колонок матрицы a * int * row - индексы начала строк матрицы a
@@ -151,7 +226,7 @@ struct CRS {
 * int &countU - размер матрицы U
 * RETURN * возвращаетс€ код ошибки **/
 
-int symbolicILUp ( int p, int n, int * col, int * row, int * &lucol, int * &lurow, double * &luval, int * uptr ) {
+int symbolicILUp ( int p, int n, int * col, int * row, int * &lucol, int * &lurow, double * &luval, int * uptr, int & countL, int & countU ) {
     int i, j, h, s, f; // счетчики циклов
     int jcol; // и временные переменные
     int * len;
@@ -160,8 +235,8 @@ int symbolicILUp ( int p, int n, int * col, int * row, int * &lucol, int * &luro
     int * adj;
     adj = new int[n];
     visited = new int[n];
-    int countL = 0;
-    int countU = 0;
+    countL = 0;
+    countU = 0;
     queue<int> Q;
     
     for ( j = 0; j < n; j++ ) {
@@ -279,14 +354,14 @@ int symbolicILUp ( int p, int n, int * col, int * row, int * &lucol, int * &luro
     }
     
     //sort index
-    //int *tCol;
-    //int *tRow;
-    //StructTranspose(n, lucol, lurow, tCol, tRow);
-    //delete []lucol;
-    //delete []lurow;
-    //StructTranspose(n, tCol, tRow, lucol, lurow);
-    //delete []tCol;
-    //delete []tRow;
+    int *tCol;
+    int *tRow;
+    StructTranspose ( n, lucol, lurow, tCol, tRow );
+    delete []lucol;
+    delete []lurow;
+    StructTranspose ( n, tCol, tRow, lucol, lurow );
+    delete []tCol;
+    delete []tRow;
     delete[] len;
     delete[] adj;
     return 0;
@@ -355,9 +430,14 @@ int numericalILUp ( int n, double * a, int * col, int * row, int * lucol, int * 
     return 0;
 }
 
-void ILUp ( CRS & matrix, CRS & LU ) {
+
+
+void ILUp ( int p, CRS & matrix, CRS & LU ) {
     int * upptr = new int[matrix.N];
-    symbolicILUp ( matrix.NZ, matrix.N, matrix.col_ind, matrix.row_ptr, LU.col_ind, LU.row_ptr, LU.val, upptr );
+    int countL, countU;
+    symbolicILUp ( p, matrix.N, matrix.col_ind, matrix.row_ptr, LU.col_ind, LU.row_ptr, LU.val, upptr, countL, countU );
+    LU.N = matrix.N;
+    LU.NZ = countL + countU;
     numericalILUp ( matrix.N, matrix.val, matrix.col_ind, matrix.row_ptr, LU.col_ind,  LU.row_ptr, upptr, LU.val );
 }
 
@@ -366,7 +446,10 @@ int main ( int argv, char *argc[] ) {
     if ( argv < 2 ) return 1;
     
     CRS matrix ( argc[1] );
+    cout << matrix << endl;
     CRS LU;
-    ILUp ( matrix, LU );
+    int p = 2;  // should be less than N
+    ILUp ( p, matrix, LU );
+    cout << LU << endl;
     return 0;
 }
